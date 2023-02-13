89cbac958377f37c3e608c258dc8820678f9d7ce
Masanori Ogawa <masanori.ogawa@awa-biz.com>
2023-02-13 23:04:12 +0900

    FooterをComponent化,prettier追加

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+// @ts-check
+
+/** @type {import('prettier').Options} */
+const prettierConfig = {
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  trailingComma: 'all',
+  arrowParens: 'avoid',
+};
+
+module.exports = prettierConfig;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,40 +1,29 @@
-import React, { FC } from "react";
-import { GetStaticProps } from "next";
+import React, { FC } from 'react';
+import { GetStaticProps } from 'next';
+import { Footer } from '@/components/Footer';
 
 type Post = {
   id: number;
   title: string;
-}
+};
 
 type Props = {
   posts: Post[];
-}
+};
 
-const Home: FC<Props> = (props) => {
+const Home: FC<Props> = props => {
   return (
     <div>
-      <h2>POSTの一覧</h2>
-      <div>
-	{props.posts.map((post, index) =>
-	  <div key={index}>
-	    <div>{post.id}.</div>
-	    <div>{post.title}</div>
-	  </div>
-        )}
-      </div>
+      <h2>Index Page</h2>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
 export const getStaticProps: GetStaticProps = async context => {
-  const response = await fetch("http://localhost/posts", {method: "GET"});
-  const json = await response.json();
-
   return {
-    props: {
-      posts: json
-    },
+    props: {},
   };
-}
+};
 
 export default Home;
